011f88724ae8bbb39a84443574ef80874f427665
kersuzananthony <kersuzan.anthony@gmail.com>
2019-11-25 13:50:19 +0100

    Test: spare ok but ugly

--- src/game.ts
+++ src/game.ts
@@ -10,9 +10,16 @@ export default class Game {
 
   public score() {
     let score = 0;
+    let i = 0;
 
-    for (let i = 0; i < this._rolls.length; i++) {
-      score += this._rolls[i];
+    for (let frame = 0; frame < 10; frame++) {
+      if (this._rolls[i] + this._rolls[i + 1] === 10) {
+        score += 10 + this._rolls[i + 2];
+      } else {
+        score += this._rolls[i] + this._rolls[i + 1];
+      }
+
+      i += 2;
     }
 
     return score;
