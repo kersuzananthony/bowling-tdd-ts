df3506dc3ed52428fc6bb4dd6516f70397fb269e
kersuzananthony <kersuzan.anthony@gmail.com>
2019-11-25 13:34:05 +0100

    Refactor: Game creation and roll many

--- tests/game.test.ts
+++ tests/game.test.ts
@@ -1,28 +1,30 @@
 import Game from "../src/game";
 
 describe("Bowling game", () => {
+
+  let game: Game | undefined;
+
+  const rollMany = (quantity: number, pinsHit: number) => {
+    for (let i = 0; i < quantity; i++) {
+      game.roll(pinsHit);
+    }
+  };
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
   it("should successfully create a bowling game", () => {
-    const game = new Game();
     expect(game).not.toBeUndefined();
   });
 
   it("should return a score equals to 0 when all balls go to gutter", () => {
-    const game = new Game();
-
-    for (let i = 0; i < 20; i++) {
-      game.roll(0);
-    }
-
+    rollMany(20, 0);
     expect(game.score()).toEqual(0);
   });
 
   it("should return a score equals to 20 because all roll hit one pin", () => {
-    const game = new Game();
-
-    for (let i = 0; i < 20; i++) {
-      game.roll(1);
-    }
-
+    rollMany(20, 1);
     expect(game.score()).toEqual(20);
   });
 });
